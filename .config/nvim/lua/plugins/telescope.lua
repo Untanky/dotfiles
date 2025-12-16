b7ec698ad76de92f0f6c89b0b8f40eb5a1f6fc3f
Lukas Grimm <lukaskingsmail@gmail.com>
2025-12-16 21:29:07 +0100

    Fixed treesitter and telscope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,67 +1,65 @@
--- TODO: consider more telescope plugins (https://github.com/nvim-telescope/telescope.nvim/wiki/Extensions#different-plugins-with-telescope-integration)
-function open_file_history() 
-  require("telescope.builtin").git_bcommits()
-end
-
 return {
   "nvim-telescope/telescope.nvim",
-  name = "Telescope",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  opts = {
-    pickers = {
-      find_files = {
-        prompt_prefix = "",
-        layout_strategy = "vertical",
-        layout_config = {
-          anchor = "N",
-          anchor_padding = 0,
-          height = 0.7,
-          width = 0.6,
-          mirror = true,
-          prompt_position = "top",
+  opts = function()
+    local sorters = require("telescope.sorters")
+
+    return {
+      pickers = {
+        find_files = {
+          prompt_prefix = "",
+          layout_strategy = "vertical",
+          layout_config = {
+            anchor = "N",
+            anchor_padding = 0,
+            height = 0.7,
+            width = 0.6,
+            mirror = true,
+            prompt_position = "top",
+          },
+          file_sorter = sorters.get_fzy_sorter(),
         },
-        file_sorter = require("telescope.sorters").get_fzy_sorter,
+
+        git_branches = {
+          prompt_prefix = " 🔍 ",
+          previewer = false,
+          layout_strategy = "vertical",
+          layout_config = {
+            anchor = "N",
+            anchor_padding = 0,
+            height = 0.4,
+            width = 0.6,
+            mirror = true,
+            prompt_position = "top",
+          },
+
+        }
       },
-
-      git_branches = {
-        prompt_prefix = " 🔍 ",
-        previewer = false,
-        layout_strategy = "vertical",
-        layout_config = {
-          anchor = "N",
-          anchor_padding = 0,
-          height = 0.4,
-          width = 0.6,
-          mirror = true,
-          prompt_position = "top",
-        },
-
-      }
-    },
-  },
+    }
+  end,
   keys = {
     -- find
-    { "<leader>ff", require("telescope.builtin").find_files,                desc = "Find: Files" },
-    { "<leader>fg", require("telescope.builtin").git_files,                 desc = "Find: Git Files" },
-    { "<leader>fb", require("telescope.builtin").buffers,                   desc = "Find: Buffers" },
-    { "<leader>fl", require("telescope.builtin").live_grep,                 desc = "Find: Everywhere" },
-    { "<leader>f/", require("telescope.builtin").current_buffer_fuzzy_find, desc = "Find: In Current Buffer" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find: Files" },
+    { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find: Git Files" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find: Buffers" },
+    { "<leader>fl", "<cmd>Telescope live_grep<cr>", desc = "Find: Everywhere" },
+    { "<leader>f/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find: In Current Buffer" },
 
     -- version control
-    { "<leader>vb", require("telescope.builtin").git_branches, desc = "Git: Branches" },
-    { "<leader>vs", require("telescope.builtin").git_status,   desc = "Git: Status" },
-    { "<leader>vh", require("telescope.builtin").git_commits,  desc = "Git: History" },
-    { "<leader>vf", require("telescope.builtin").git_bcommits, desc = "Git: File History" },
-    { "<leader>vt", require("telescope.builtin").git_stash,    desc = "Git: Stashes" },
-    
-    -- lsp / language
-    { "<leader>lr", require("telescope.builtin").lsp_references,        desc = "LSP: References" },
-    { "<leader>ld", require("telescope.builtin").lsp_definitions,       desc = "LSP: Definitions" },
-    { "<leader>li", require("telescope.builtin").lsp_implementations,   desc = "LSP: Implementations" },
-    { "<leader>lt", require("telescope.builtin").lsp_type_definitions,  desc = "LSP: Type Definitions" },
-    { "<leader>ls", require("telescope.builtin").lsp_document_symbols,  desc = "LSP: Document Symbols" },
-    { "<leader>lS", require("telescope.builtin").lsp_workspace_symbols, desc = "LSP: Workspace Symbols" },
+    { "<leader>vb", "<cmd>Telescope git_branches<cr>", desc = "Git: Branches" },
+    { "<leader>vs", "<cmd>Telescope git_status<cr>", desc = "Git: Status" },
+    { "<leader>vh", "<cmd>Telescope git_commits<cr>", desc = "Git: History" },
+    { "<leader>vf", "<cmd>Telescope git_bcommits<cr>", desc = "Git: File History" },
+    { "<leader>vt", "<cmd>Telescope git_stash<cr>", desc = "Git: Stashes" },
+
+    -- lsp
+    { "<leader>lr", "<cmd>Telescope lsp_references<cr>", desc = "LSP: References" },
+    { "<leader>ld", "<cmd>Telescope lsp_definitions<cr>", desc = "LSP: Definitions" },
+    { "<leader>li", "<cmd>Telescope lsp_implementations<cr>", desc = "LSP: Implementations" },
+    { "<leader>lt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "LSP: Type Definitions" },
+    { "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "LSP: Document Symbols" },
+    { "<leader>lS", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "LSP: Workspace Symbols" },
   },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  name = "Treesitter",
   build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = {
-        "lua", "vim", "vimdoc", "javascript", "html", "typescript", "css", "dockerfile", "go", "gosum", "gomod", "json",
-        "yaml", "git_rebase", "gitcommit", "gitignore", "terraform"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  opts = {
+    ensure_installed = {
+      "lua", "vim", "vimdoc", "javascript", "html", "typescript", "css", "dockerfile", "go", "gosum", "gomod", "json",
+      "yaml", "git_rebase", "gitcommit", "gitignore", "terraform"
+    },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+  }
 }
